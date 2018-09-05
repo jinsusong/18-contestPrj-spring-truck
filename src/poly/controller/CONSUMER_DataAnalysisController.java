382b42d92a282851d03fdd99643c1e28434f6a20
IMerCw <pierrotcw@gmail.com>
2018-09-05 20:28:09 +0900

    2018.09.05 PCW 개인메뉴추천 시스템 개발 중

--- src/poly/controller/CONSUMER_DataAnalysisController.java
+++ src/poly/controller/CONSUMER_DataAnalysisController.java
@@ -36,6 +36,7 @@ import poly.dto.consumer.CONSUMER_RcmmndMenuDTO;
 import poly.dto.consumer.CONSUMER_UserDTO;
 import poly.service.CONSUMER_IFtService;
 import poly.util.CmmUtil;
+import poly.util.CosineSimilarity;
 import poly.util.GeoUtil;
 import poly.util.RServe;
 import poly.util.CONSUMER_UtilFile;
@@ -45,6 +46,11 @@ import poly.service.CONSUMER_IImageService;
 import poly.service.CONSUMER_IUserService;
 import poly.service.impl.CONSUMER_ImageService;
 import poly.service.impl.CONSUMER_UserService;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /*
  * Controller 선언해야만 Spring 프레임워크에서 Controller인지 인식 가능
@@ -57,6 +63,7 @@ public class CONSUMER_DataAnalysisController {
 	@Resource(name = "CONSUMER_FtService")
 	private CONSUMER_IFtService ftService;
 	
+	//트럭왔냠의 추천 메뉴
 	@RequestMapping(value="consumer/rcmmnd/rcmmndMenu", method=RequestMethod.GET)
 	public String rcmmndMenu(HttpServletRequest request, Model model) throws Exception{
 			log.info("Access rcmmndMenu.........");
@@ -71,6 +78,32 @@ public class CONSUMER_DataAnalysisController {
 			return "/consumer/rcmmnd/rcmmndMenu";
 	}
 	
+	//소비자 맞춤 추천 메뉴
+	@RequestMapping(value="consumer/rcmmnd/CustomRcmmnd")
+	public String CustomRcmmnd(HttpServletRequest request, Model model) throws Exception{
+		log.info("Access CustomRcmmnd...");
+		CosineSimilarity cossim = new CosineSimilarity();
+		Map<CharSequence, Integer> leftVector = new HashMap<>();
+		Map<CharSequence, Integer> rightVector= new HashMap<>();
+		leftVector.put("수학", 10);
+		leftVector.put("영어", 4);
+		leftVector.put("과학", 0);
+		leftVector.put("국어", 0);
+		
+		rightVector.put("수학", 10);
+		rightVector.put("영어", 6999);
+		rightVector.put("과학", 9999);
+		rightVector.put("국어", 10000);
+		
+		
+		
+		Double result = cossim.cosineSimilarity(leftVector, rightVector);
+		
+		log.info("The cosine Similariry of two vectors is " + result);
+		
+		log.info("Terminate CustomRcmmnd...");
+		return "/consumer/rcmmnd/customRcmmnd";
+	}
 	
 	
 }
